aecdbe2190c9f8a810e070e2f2fce5d7266d69e4
duardopx <duardopx@protonmail.com>
2021-06-16 14:12:04 -0300

    feat: set alchemy keys on hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
+require("@nomiclabs/hardhat-waffle");
+
 /**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.7.3",
-};
+  * @type import('hardhat/config').HardhatUserConfig
+  */
+  module.exports = {
+    defaultNetwork: "hardhat",
+    solidity: "0.8.3",
+    networks: {
+      ropsten: {
+        url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+        accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+      }
+    },
+
+  };
