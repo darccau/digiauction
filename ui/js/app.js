0ac698ce686432567528b0e77b7dd6c1304f1ff1
duardopx <darccau@protonmail.com>
2021-06-20 17:45:57 -0300

    Vertical listation of pokemons

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,4 +1,5 @@
-const listDigimons = document.querySelector('#list-digimon')
+const listDigimons = document.querySelector('.digimon-cards')
+
 const digimonAPI = 'https://digimon-api.vercel.app/api/digimon'
 
 const digimons = fetch(digimonAPI).then(response => response.json())
@@ -7,19 +8,18 @@ digimons.then(digimons => {
   const digimonQuery = digimons.reduce((accumulator, digimon) => {
     accumulator += `
       <li class="card">
-      <img src="${digimon.img}"> 
-      <h2><strong>${digimon.name}strong: ${digimon.level}</h2>
+      <img class="card-image" src="${digimon.img}"> 
+      <h2 class="card-title"><strong>${digimon.name}</strong>: ${digimon.level}</h2>
       <h2>Biggest bet: 123123</h2>
       <form action="#action">
-      <h2>make your bet</h2>
-      <input type="text"></input>
+      <h2 class="card-title">make your bet</h2>
+      <input class="card-subtitle" type="text"></input>
       </form>
-      <p>fuck that shit</p>
       </li>
       `  
     return accumulator
   }, '')
-  console.log(listDigimons)
+
   listDigimons.innerHTML = digimonQuery
 })
 
