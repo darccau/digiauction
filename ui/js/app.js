f80516ab5be957c629b81fb9530ca05e5fd17bc6
duardopx <darccau@protonmail.com>
2021-06-28 22:46:15 -0300

    add: add metamask interaction on app

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,6 +1,54 @@
-const listDigimons = document.querySelector('.digimon-cards')
+// const contractAddress = "";
 
+const listDigimons = document.querySelector('.digimon-cards')
 const digimonAPI = 'https://digimon-api.vercel.app/api/digimon'
+
+const DApp = {
+  web3: null,
+  contracts: {},
+  account: null,
+
+  init: function () {
+    return DApp.initWeb3();
+  },
+
+  initWeb3: async function () {
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        const accounts = await window.ethereum.request({ // Requisita primeiro acesso ao Metamask
+          method: "eth_requestAccounts",
+        });
+        DApp.account = accounts[0];
+        window.ethereum.on('accountsChanged', DApp.updateAccount); // Atualiza se o usuário trcar de conta no Metamaslk
+      } catch (error) {
+        console.error("Usuário negou acesso ao web3!");
+        return;
+      }
+      DApp.web3 = new Web3(window.ethereum);
+    } else {
+      console.error("Instalar MetaMask!");
+      return;
+    }
+    return DApp.initContract();
+  },
+
+  // Atualiza 'DApp.account' para a conta ativa no Metamask
+  updateAccount: async function() {
+    DApp.account = (await DApp.web3.eth.getAccounts())[0];
+    atualizaInterface();
+  },
+
+  // Associa ao endereço do seu contrato
+  initContract: async function () {
+    DApp.contracts.Contrato = new DApp.web3.eth.Contract(abi, contractAddress);
+    return DApp.render();
+  },
+
+  // Inicializa a interface HTML com os dados obtidos
+  render: async function () {
+    inicializaInterface();
+  },
+}
 
 const digimons = fetch(digimonAPI).then(response => response.json())
 
@@ -10,11 +58,11 @@ digimons.then(digimons => {
       <li class="card">
       <img class="card-image" src="${digimon.img}"> 
       <h2 class="card-title">${digimon.name}</h2>
-      <h3>Level: ${digimon.level}</h3>
-      <h2>Biggest bet: 123123</h2>
+      <h4>Level: ${digimon.level}</h4>
+      <h4>Biggest bet: 123123</h4>
       <form action="#action">
-      <h2 class="card-title">make your bet</h2>
-      <input class="card-subtitle" type="text"></input>
+      <h4>make your bet</h4>
+      <input placeholder="fuc"class="card-subtitle" type="text"></input>
       </form>
       </li>
       `  
