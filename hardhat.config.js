5c1822691a7360c22f8d497d43487af41b27dcfc
duardopx <darccau@protonmail.com>
2021-06-30 20:16:31 -0300

    add: update hardhat settings

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
 /**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.7.3",
-};
+  * @type import('hardhat/config').HardhatUserConfig
+  */
+  module.exports = {
+    solidity: "0.8.3",
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.alchemyapi.io/v2/<key>"
+      }
+    }
+  };
